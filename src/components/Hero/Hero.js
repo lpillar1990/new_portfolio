c56cb8f4199c7a787492d450155cde1fb7c74f0d
leopillar <lpillar1990@googlemail.com>
2023-01-16 16:18:30 +0000

    cv update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import { Section, SectionText, SectionTitle, Link } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import React from "react";
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+  Link,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
   <>
@@ -11,15 +16,20 @@ const Hero = (props) => (
           Hey there,
         </SectionTitle>
         <SectionText>
-          My name is Leo, I am a Front-End Developer from Bangor, Northern Ireland.
-          My path has taken a few changes over the years - from doing 3 years I.C.T
-          in college, to the last 12 years being a Commercial Diver. My passion and
-          interest in web development has led me back to being a Front-end Developer.
+          My name is Leo, I am a Front-End Developer from Bangor, Northern
+          Ireland. My path has taken a few changes over the years - from doing 3
+          years I.C.T in college, to the last 12 years being a Commercial Diver.
+          My passion and interest in web development has led me back to being a
+          Front-end Developer.
         </SectionText>
-        <Button onClick={(e) => {
-          e.preventDefault();
-          window.location.href = '/files/resume.pdf';
-        }}>My Resume</Button>
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = "/files/resume.pdf";
+          }}
+        >
+          My CV
+        </Button>
       </LeftSection>
     </Section>
   </>
